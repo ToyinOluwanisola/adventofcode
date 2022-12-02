c8e170bb5d867aa576b20bf02367f16b63bc02bd
Toyin Oluwanisola <101352281+ToyinOluwanisola@users.noreply.github.com>
2022-12-02 12:06:27 +0000

    part 1

---
+++ 2022/day2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	rockScore     = 1
+	paperScore    = 2
+	scissorsScore = 3
+	win           = 6
+	draw          = 3
+	lost          = 0
+)
+
+func main() {
+
+	file, err := os.Open("file.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	totalScore := 0
+
+	for scanner.Scan() {
+		value := scanner.Text()
+		results := strings.Split(value, " ")
+		score := 0
+
+		switch results[0] {
+		case "A": // Rock
+			if results[1] == "X" { //rock=draw
+				score += rockScore + draw
+			} else if results[1] == "Y" { //paper=win
+				score += paperScore + win
+			} else if results[1] == "Z" { //scissors=lost
+				score += scissorsScore + lost
+			}
+		case "B": // Paper
+			if results[1] == "X" { //rock=lost
+				score += rockScore + lost
+			} else if results[1] == "Y" { //paper=draw
+				score += paperScore + draw
+			} else if results[1] == "Z" { //scissors=win
+				score += scissorsScore + win
+			}
+		case "C": // Scissors
+			if results[1] == "X" { //rock=win
+				score += rockScore + win
+			} else if results[1] == "Y" { //paper=lost
+				score += paperScore + lost
+			} else if results[1] == "Z" { //scissors=draw
+				score += scissorsScore + draw
+			}
+		}
+		totalScore += score
+		//fmt.Println("score is ", score)
+	}
+
+	fmt.Println("total score is ", totalScore)
+}
